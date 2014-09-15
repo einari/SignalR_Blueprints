ba7bf5119f33b5354bf313cec8c673dfb320fcc5
Einar Ingebrigtsen <einar@dolittle.com>
2014-09-15 20:49:28 +0200

    Tidying up duplicate code.

--- Source/Chapter4/index.js
+++ Source/Chapter4/index.js
@@ -7,53 +7,40 @@
         var hub = $.connection.requestStatisticsHub;
         var initializedCount = 0;
 
-        function handleRequest(key, value) {
-            var result = self.requests().forEach(function (request) {
-                if (request[0] == key) {
-                    request[1](value);
+        function handleEntry(log, key, value) {
+            var result = log().forEach(function (entry) {
+                if (entry[0] == key) {
+                    entry[1](value);
                     return true;;
                 }
             });
 
             if (result !== true) {
-                self.requests.push([key, ko.observable(value)]);
-            }
-        };
-
-        function handleFailedRequest(key, value) {
-            var result = self.failedRequests().forEach(function (request) {
-                if (request[0] == key) {
-                    request[1](value);
-                    return true;;
-                }
-            });
-
-            if (result !== true) {
-                self.failedRequests.push([key, ko.observable(value)]);
+                log.push([key, ko.observable(value)]);
             }
         };
 
         hub.client.requestCountChanged = function (key, value) {
             if (initializedCount < 2) return;
-            handleRequest(key, value);
+            handleEntry(self.requests, key, value);
         }
 
         hub.client.failedRequestCountChanged = function (key, value) {
             if (initializedCount < 2) return;
-            handleFailedRequest(key, value);
+            handleEntry(self.failedRequests, key, value);
         }
 
         $.connection.hub.start().done(function () {
             hub.server.getRequests().done(function (requests) {
                 for (var property in requests) {
-                    handleRequest(property, requests[property]);
+                    handleEntry(self.requests, property, requests[property]);
                 }
 
                 initializedCount++;
             });
             hub.server.getFailedRequests().done(function (requests) {
                 for (var property in requests) {
-                    handleFailedRequest(property, requests[property]);
+                    handleEntry(self.failedRequests, property, requests[property]);
                 }
 
                 initializedCount++;
